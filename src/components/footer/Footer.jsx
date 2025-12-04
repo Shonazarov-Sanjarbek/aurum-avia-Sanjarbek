12ed839e1b8d24e19c5ba46a9cf2035ee6369ab4
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2025-12-04 22:05:08 +0500

    aurum-avia

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,63 @@
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTelegramPlane,
+  FaPhoneAlt,
+  FaEnvelope,
+  FaPlaneDeparture,
+} from "react-icons/fa";
+
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
-        © 2025 Aviakassa — Barcha huquqlar himoyalangan.
+      <div className="footer-container">
+        {/* Logo */}
+        <div className="footer-logo">
+          <div className="logo">
+            <FaPlaneDeparture className="logo-icon" />
+            AURUM<span>AVIA</span>
+          </div>
+          <p>© 2025 Aviakassa — Barcha huquqlar himoyalangan.</p>
+        </div>
+
+        <div className="footer-wrapper">
+        {/* Sayt bo‘limlari */}
+        <div className="footer-links">
+          <h3>Sayt bo‘limlari</h3>
+          <p>Bosh sahifa</p>
+          <p>Xizmatlar</p>
+          <p>Biz haqimizda</p>
+          <p>Aloqa</p>
+        </div>
+
+        {/* Aloqa */}
+        <div className="footer-contact">
+          <h3>Aloqa</h3>
+          <div className="contact-item">
+            <FaPhoneAlt /> <a href="tel:+998909870987">+998 90 987 09 87</a>
+          </div>
+          <div className="contact-item">
+            <FaEnvelope /> <span>info@aviakassa.uz</span>
+          </div>
+          <div className="social-icons">
+            <a href="#">
+              <FaFacebookF />
+            </a>
+            <a href="#">
+              <FaInstagram />
+            </a>
+            <a href="#">
+              <FaTelegramPlane />
+            </a>
+          </div>
+        </div>
+
+        </div>
+      </div>
+
+      {/* Pastki qism */}
+      <div className="footer-bottom">
+        Sayt <span>San_Dev</span> tomonidan yaratilgan
       </div>
     </footer>
   );
